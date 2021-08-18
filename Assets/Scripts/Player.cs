98329b34055e3f309e86f7a9ad1d18f6e3112f1d
Jim Gagliardi <jvgagliardi98@outlook.com>
2021-08-18 19:53:00 -0400

    Created player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _spd = 5.0f;
+    private Vector3 _velocity;
+}
