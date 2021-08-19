5fb452fa2e97bdeed02c928a83fa24dac7530b4d
Jim Gagliardi <jvgagliardi98@outlook.com>
2021-08-19 19:45:34 -0400

    Created basic player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,4 +6,19 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float _spd = 5.0f;
     private Vector3 _velocity;
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float hInput = Input.GetAxisRaw("Horizontal");
+        float vInput = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(hInput, vInput, 0.0f);
+
+        _velocity = direction * _spd;
+        transform.Translate(_velocity * Time.deltaTime);
+    }
 }
