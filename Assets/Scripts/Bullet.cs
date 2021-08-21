bff32ef0585a6271eb4be79dd1a31d298f59d939
Jim Gagliardi <jvgagliardi98@outlook.com>
2021-08-20 20:10:55 -0400

    Created bullet prefab and function

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float _spd = 15.0f; // Speed of the Bullet
+    
+    private void Update()
+    {
+        BulletMovement();  // Call Bullet Method
+    }
+
+    private void BulletMovement()
+    {
+        Vector3 bulletVelocity = Vector3.right * _spd;      //Set Bullet Direction
+        transform.Translate(bulletVelocity * Time.deltaTime); // Move Bullet
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,21 +4,45 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _spd = 5.0f;
-    private Vector3 _velocity;
+    [SerializeField] private float _spd = 5.0f; // Player Movement Speed
+    [SerializeField] private float _fireRate = 1.0f; //Fire Rate of weapon
+    private float _cycleTime = 0.0f; //Cycle time for weapon
+    private Vector3 _velocity; //Velocity for player
+    [SerializeField] private GameObject _bullet; //Bullet Player Shoots
 
     private void Update()
     {
-        PlayerMovement();
+        PlayerMovement(); // Move player
+        PlayerShoot(); //Player Shoot
     }
 
     void PlayerMovement()
     {
-        float hInput = Input.GetAxisRaw("Horizontal");
+        float hInput = Input.GetAxisRaw("Horizontal"); //Get input and use it to set direction
         float vInput = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(hInput, vInput, 0.0f);
 
-        _velocity = direction * _spd;
-        transform.Translate(_velocity * Time.deltaTime);
+        _velocity = direction * _spd; //Create velocity
+        transform.Translate(_velocity * Time.deltaTime); //Move player
+    }
+
+    void PlayerShoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) //Check forr space bar
+        {
+            if (Time.time > _cycleTime) //if game time is greater than weapon cycle time
+            {
+                _cycleTime = Time.time + _fireRate; //Set next cycle time
+                
+                if (_bullet != null) //Fire bullet if it exists
+                {
+                    Instantiate(_bullet, transform.position, Quaternion.identity);
+                }
+                else
+                {
+                    Debug.LogError("The BULLET is null");
+                }
+            }
+        }
     }
 }
